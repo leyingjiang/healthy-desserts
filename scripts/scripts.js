65831b8a7eb2d5828d6bbe4e21c51865b279723b
leyingjiang <leyingj@bu.edu>
2022-12-02 18:04:26 -0500

    update-leying

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,43 +1,61 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
 
 /* calories related choices  */
-const fruitFBChoice = Array.from(document.getElementsByName("fruitFBChoice"));
-const extraFBChoice = Array.from(document.getElementsByName("extraFBChoice"));
-const flourCChoice = Array.from(document.getElementsByName("flourCChoice"));
-const extraCChoice = Array.from(document.getElementsByName("extraCChoice"));
-const beanBChoice = Array.from(document.getElementsByName("BeanBChoice"));
-const extraBChoice = Array.from(document.getElementsByName("extraBChoice"));
+const fruitChoiceFB = Array.from(document.getElementsByName("fruit-choice-fb"));
+const extraChoiceFB = Array.from(document.getElementsByName("extra-choice-fb"));
+const flourChoiceC = Array.from(document.getElementsByName("flour-choice-c"));
+const extraChoiceC = Array.from(document.getElementsByName("extra-choice-c"));
+const beanChoiceB = Array.from(document.getElementsByName("bean-choice-b"));
+const extraChoiceB = Array.from(document.getElementsByName("extra-choice-c"));
 
-const caloriesChoice = Array.from(fruitFBChoice, extraFBChoice, flourCChoice, extraCChoice, beanBChoice, extraBChoice);
+const FirstChoice = Array.from(fruitChoiceFB, flourChoiceC, beanChoiceB);
+const SecondChoice = Array.from(extraChoiceFB, extraChoiceC, extraChoiceB);
 
 /* inputs */
 const performanceInput = document.querySelector("#performance");
 const caloriesInput = document.querySelector("#calories");
-const adviceInput = document.querySelector("#advice");
-const recipeInput = document.querySelector("#recipe");
 
 /* buttons add eventlistener */
-caloriesChoice[0].addEventListener("change", applyCalories);
-caloriesChoice[1].addEventListener("change",applyCalories);
-caloriesChoice[2].addEventListener("change",applyCalories);
-caloriesChoice[3].addEventListener("change",applyCalories);
-caloriesChoice[4].addEventListener("change",applyCalories);
-caloriesChoice[5].addEventListener("change",applyCalories);
+FirstChoice[0].addEventListener("change", getFirstChoice);
+FirstChoice[1].addEventListener("change",getFirstChoice);
+FirstChoice[2].addEventListener("change",getFirstChoice);
+FirstChoice[3].addEventListener("change",getFirstChoice);
+FirstChoice[4].addEventListener("change",getFirstChoice);
+FirstChoice[5].addEventListener("change",getFirstChoice);
+SecondChoice[0].addEventListener("change",getSecondChoice);
+SecondChoice[1].addEventListener("change",getSecondChoice);
+SecondChoice[2].addEventListener("change",getSecondChoice);
+SecondChoice[3].addEventListener("change",getSecondChoice);
+SecondChoice[4].addEventListener("change",getSecondChoice);
+SecondChoice[5].addEventListener("change",getSecondChoice);
 
 /* functions */
 
-function calculateTotal(currentCalories){
-    caloriesInput.value= currentCalories + currentCalories;
+function getFirstChoice() {
+    for(var i=0; i< FirstChoice.length;i++){
+        if(FirstChoice[i].checked){
+            FirstCalories += FirstChoice[i].value;
+        }
+    }
+    return FirstCalories;
 }
 
-function applyCalories(){
-    let currentCalories = null;
-    caloriesChoice.forEach((item) =>
-    {
-        if(item.checked){
-            currentCalories = item.value;
+function getSecondChoice() {
+    for(var i=0; i<SecondChoice.length;i++){
+        if(SecondChoice[i].checked){
+            SecondCalories += SecondChoice[i].value;
         }
-    });
-    calculateTotal(currentCalories);
+        return SecondCalories;
+    }
+
 }
+
+function getTotalCalories() {
+    caloriesInput = getFirstChoice() + getSecondChoice();
+}
+
+
+
+
+
 
